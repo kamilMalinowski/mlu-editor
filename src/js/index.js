99e8340910ff3b34e83430d6b89b133f8186ec20
Kamil Malinowski <kamil.malinowski.mlu@gmail.com>
2020-08-04 20:46:55 +0200

    make some improvments

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,8 @@ import "../scss/main.scss";
 
 /* place your code below */
 
+
+/* save & load */
 const save = document.querySelector(".save--js");
 const textarea = document.querySelector(".textarea--js");
 const load = document.querySelector(".load--js");
@@ -18,3 +20,13 @@ load.addEventListener("click", (e) => {
   e.preventDefault();
   textarea.value = localStorage.getItem("entry");
 });
+
+/* counter */
+const input = document.querySelector(".textarea--js"),
+  charCount = document.querySelector(".charCount--js");
+
+const onKeyDown = function () {
+  charCount.textContent = input.value.length;
+};
+
+input.addEventListener("keyup", onKeyDown);
