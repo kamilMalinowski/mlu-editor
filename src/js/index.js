775be2984edcf4a27fe94d548e01194e61c688cf
Kamil Malinowski <kamil.malinowski.mlu@gmail.com>
2020-09-01 00:18:42 +0200

    feat: update all styles and html, add saveCount JS function, delete media queries

--- src/js/index.js
+++ src/js/index.js
@@ -8,18 +8,33 @@ const load = document.querySelector(".load--js");
 save.addEventListener("click", (e) => {
   e.preventDefault();
   localStorage.setItem("entry", textarea.value);
+
 });
 load.addEventListener("click", (e) => {
   e.preventDefault();
   textarea.value = localStorage.getItem("entry");
 });
 
-/* counter */
+/* text counter */
 const input = document.querySelector(".textarea--js"),
-  charCount = document.querySelector(".charCount--js");
+charCount = document.querySelector(".charCount--js");
 
 const onKeyDown = function () {
   charCount.textContent = input.value.length;
 };
 
 input.addEventListener("keyup", onKeyDown);
+
+/* save counter --TEST!!!!! */
+var count = 0;
+var countButton = document.querySelector(".save--js");
+var displayCount = document.querySelector(".saveCount--js");
+countButton.onclick = function(){
+  count++;
+  displayCount.innerHTML = count;
+}
+
+
+
+
+
